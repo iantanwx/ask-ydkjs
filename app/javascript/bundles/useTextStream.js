1a13c56e146705a393ff8e66fa2d8789b76168af
iantanwx <ian.tan@voyage.finance>
2023-05-01 16:20:50 +0800

    fix race condition in hook

--- app/javascript/bundles/useTextStream.js
+++ app/javascript/bundles/useTextStream.js
@@ -1,23 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const useTextStream = (text) => {
   const words = text.split(" ");
   const [currentText, setCurrentText] = useState("");
   const [wordIndex, setWordIndex] = useState(0);
+  const activeText = useRef(text);
 
   useEffect(() => {
     setCurrentText("");
     setWordIndex(0);
+    activeText.current = text;
   }, [text]);
 
   useEffect(() => {
     if (!text || !words.length) return;
-    setCurrentText((t) => `${t} ${words[wordIndex] ? words[wordIndex] : ""}`);
-    if (wordIndex < words.length - 1) {
-      setTimeout(() => {
-        setWordIndex((i) => i + 1);
-      }, Math.random() * 50 + 50);
-    }
+
+    const updateWord = () => {
+      if (activeText.current === text) {
+        setCurrentText(
+          (t) => `${t} ${words[wordIndex] ? words[wordIndex] : ""}`
+        );
+        if (wordIndex < words.length - 1) {
+          setWordIndex((i) => i + 1);
+        }
+      }
+    };
+
+    const timerId = setTimeout(updateWord, Math.random() * 50 + 50);
+
+    // Cleanup function to avoid the race condition
+    return () => {
+      clearTimeout(timerId);
+    };
   }, [text, wordIndex]);
 
   return [currentText, wordIndex < words.length - 1];
